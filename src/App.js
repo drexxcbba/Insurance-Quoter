f0aa5997e29835c42bb7a6e49c4d049ae3cf4c61
unknown <villarroel24kyle@gmail.com>
2021-01-22 14:13:08 -0400

    Adding styled components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import Header from './components/Header';
+import styled from '@emotion/styled';
+import Form from './components/Form';
+
+const Container = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContainerForm = styled.div`
+  background-color: #FFF;
+  padding: 3rem;
+`;
 
 function App() {
   return (
-    <Header
-     titulo="Cotizador de seguros" 
-     />
+    <Container>
+      <Header
+        titulo="Cotizador de seguros" 
+      />
+      <ContainerForm>
+        <Form />
+      </ContainerForm>
+    </Container>
   );
 }
 
